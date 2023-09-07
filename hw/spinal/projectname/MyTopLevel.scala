38bd7e319a065472b4e7f9fb615376fd9356e0fc
hy0000 <2877079184@qq.com>
2023-09-07 22:12:10 +0800

    slave error reproduce

--- hw/spinal/projectname/MyTopLevel.scala
+++ hw/spinal/projectname/MyTopLevel.scala
@@ -1,24 +1,18 @@
 package projectname
 
 import spinal.core._
+import spinal.lib._
 
-// Hardware definition
+case class P() extends Bundle with IMasterSlave {
+  val a = Bool()
+  override def asMaster(): Unit = {
+    in(a)
+  }
+}
+
 case class MyTopLevel() extends Component {
-  val io = new Bundle {
-    val cond0 = in  Bool()
-    val cond1 = in  Bool()
-    val flag  = out Bool()
-    val state = out UInt(8 bits)
-  }
-
-  val counter = Reg(UInt(8 bits)) init 0
-
-  when(io.cond0) {
-    counter := counter + 1
-  }
-
-  io.state := counter
-  io.flag := (counter === 0) | io.cond1
+  val p = slave(P())
+  p.a := True
 }
 
 object MyTopLevelVerilog extends App {
